ff894d3a1abcdbeadff7e8c5b288ce1c30194215
Rashed Al-Masri <157921405+BloodyFlakes@users.noreply.github.com>
2024-09-22 16:55:31 +0300

    Add new problems

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // 2620. Counter
-// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
-
-// Solution:
 const createCounter = function (n) {
   return function counter() {
     return n++;
@@ -10,19 +7,66 @@ const createCounter = function (n) {
 
 // ---------------------------------------------------------------------------------------------------
 // 2621. Sleep
-// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value. It should return a promise that resolves after 100ms.
-
-// Solution:
 const sleep = async function sleep(millis) {
   await new Promise((resolve) => setTimeout(resolve, millis));
 };
 
 // ---------------------------------------------------------------------------------------------------
 // 2619. Array Prototype Last
-// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
-// The Array.prototype.last() method is designed to return the last element of an array, or -1 if the array is empty. This can be useful in scenarios where you want to quickly access the last element of an array without having to write additional code to handle empty arrays.
 
 // Solution:
 Array.prototype.last = function () {
   return this.length > 0 ? this[this.length - 1] : -1;
 };
+
+// ---------------------------------------------------------------------------------------------------
+// 2665. Counter II
+// const createCounter = function (init) {
+//   let p = init;
+//   return {
+//     increment: () => ++p,
+//     decrement: () => --p,
+//     reset: () => (p = init),
+//   };
+// };
+
+// ---------------------------------------------------------------------------------------------------
+// 2626. Array Reduce Transformation
+const reduce = function (nums, fn, init) {
+  let x = init;
+  for (let i = 0; i < nums.length; i++) {
+    x = fn(x, nums[i]);
+  }
+  return x;
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2634. Filter Elements from Array
+const filter = function (arr, fn) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2629. Function Composition
+const compose = function (functions) {
+  if (functions.length === 0) {
+    return function (x) {
+      return x;
+    };
+  }
+
+  return functions.reduceRight(function (prevFn, nextFn) {
+    return function (x) {
+      return nextFn(prevFn(x));
+    };
+  });
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2634. Filter Elements from Array
