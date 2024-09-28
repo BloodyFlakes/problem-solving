c285335080e067d8a62278c0c178c2135e2ff9b8
Rashed Al-Masri <157921405+BloodyFlakes@users.noreply.github.com>
2024-09-28 20:21:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const sleep = async function sleep(millis) {
 
 // ---------------------------------------------------------------------------------------------------
 // 2619. Array Prototype Last
-
-// Solution:
 Array.prototype.last = function () {
   return this.length > 0 ? this[this.length - 1] : -1;
 };
@@ -69,4 +67,148 @@ const compose = function (functions) {
 };
 
 // ---------------------------------------------------------------------------------------------------
-// 2634. Filter Elements from Array
+// 2635. Apply Transform Over Each Element in Array (First solved problem without any help and from the first time )
+const map = function (arr, fn) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const val = fn(arr[i], i);
+    newArr.push(val);
+  }
+  return newArr;
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2667. Create Hello World Function
+const createHelloWorld = function () {
+  return function (...args) {
+    return 'Hello World';
+  };
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2677. Chunk Array
+const chunk = function (arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+
+  return newArr;
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2703. Return Length of Arguments Passed
+const argumentsLength = function (...args) {
+  return args.length;
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2723. Add Two Promises
+const addTwoPromises = async function (promise1, promise2) {
+  const [v1, v2] = await Promise.all([promise1, promise2]);
+
+  return v1 + v2;
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2704. To Be Or Not To Be
+const expect = function (val) {
+  return {
+    toBe: (params) => {
+      if (val !== params) throw new Error('Not Equal');
+      else return true;
+    },
+    notToBe: (params) => {
+      if (val === params) throw new Error('Equal');
+      else return true;
+    },
+  };
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2666. Allow One Function Call
+const once = function (fn) {
+  let hasBeenCalled = false;
+  let result;
+  return function (...args) {
+    if (!hasBeenCalled) {
+      result = fn(...args);
+      hasBeenCalled = true;
+      return result;
+    } else {
+      return undefined;
+    }
+  };
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2695. Array Wrapper
+const ArrayWrapper = function (nums) {
+  this.array = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function () {
+  return this.array.reduce((acc, cur) => acc + cur, 0);
+};
+
+ArrayWrapper.prototype.toString = function () {
+  return JSON.stringify(this.array);
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2724. Sort By
+const sortBy = function (arr, fn) {
+  return arr.sort((a, b) => fn(a) - fn(b));
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2648. Generate Fibonacci Sequence
+const fibGenerator = function* () {
+  let first = 0;
+  let second = 1;
+
+  while (true) {
+    yield first;
+    let value = first + second;
+    first = second;
+    second = value;
+  }
+};
+
+// ---------------------------------------------------------------------------------------------------
+// 2726. Calculator with Method Chaining
+class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+  add(value) {
+    this.result += value;
+    return this;
+  }
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+  divide(value) {
+    if (value === 0) {
+      throw new Error('Division by zero is not allowed');
+    } else {
+      this.result /= value;
+      return this;
+    }
+  }
+  power(value) {
+    this.result = Math.pow(this.result, value);
+    return this;
+  }
+  getResult() {
+    return this.result;
+  }
+}
+
+// ---------------------------------------------------------------------------------------------------
+// 2726. Calculator with Method Chaining
